25003da6faf89f2d1cbf54e9abf3f6aa8c401740
sontrinh16 <trinhleson2000@gmail.com>
2025-05-05 10:48:07 +0700

    add util for utxo hash

--- types/src/utils.rs
+++ types/src/utils.rs
@@ -25,3 +25,14 @@ pub fn generate_nullifier(viewing_key: Vec<u8>, leaf_index: u64) -> Vec<u8> {
     let leaf_index_bytes = leaf_index.to_le_bytes().to_vec();
     sha256(vec![nullifying_key.as_slice(), leaf_index_bytes.as_slice()])
 }
+
+pub fn generate_utxo_hash(random: Vec<u8>, master_pubkey: Vec<u8>, token_id: Vec<u8>, amount: u64) -> Vec<u8> {
+    let uxto_pubkey = sha256(vec![master_pubkey.as_slice(), random.as_slice()]);
+    let value: Vec<u8> = amount.to_le_bytes().to_vec();
+
+    sha256(vec![
+        uxto_pubkey.as_slice(),
+        token_id.as_slice(),
+        value.as_slice(),
+    ])
+}
